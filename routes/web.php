57692778d8b31e316bb4c0d56e9d1f47d7790ebc
Hiperion09 <juanesteban.arenas2002@gmail.com>
2024-03-05 19:30:32 -0500

    tienda virtual

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/products', [ProductController::class,'index']);
+
+Route::get('/formulario', function () {
+    echo "Hola estamos en formulario";
 });
